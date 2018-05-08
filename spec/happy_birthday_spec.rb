e88966de5a0ac7ee6d78f9e213e99ce798ba27cb
takanamito <takanamito0928@gmail.com>
2018-05-09 00:02:57 +0900

    Add specs

--- spec/happy_birthday_spec.rb
+++ spec/happy_birthday_spec.rb
@@ -1,9 +1,23 @@
+require 'spec_helper'
+
 RSpec.describe HappyBirthday do
-  it "has a version number" do
+  it 'has a version number' do
     expect(HappyBirthday::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.born_on' do
+    subject { HappyBirthday.born_on(birthday) }
+
+    context 'when parameter is Date object' do
+      let(:birthday) { Date.today }
+
+      it { is_expected.to be_a(HappyBirthday::Birthday) }
+    end
+
+    context 'when parameter is String date' do
+      let(:birthday) { '2018-05-08' }
+
+      it { is_expected.to be_a(HappyBirthday::Birthday) }
+    end
   end
 end
